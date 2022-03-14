0288ddd85c7df3cc2a797db288444a37eafe7e14
Anita Jamgbadi <jamgbanita@gmail.com>
2022-03-14 03:39:22 -0700

    fingers crossed casue of betty"

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main (void)
+
+/** THis is the main entry point
+ *
+ */
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return(0);
 }
