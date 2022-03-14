18ccb07a89ea22a079232d142b3ade536d19dfa8
Anita Jamgbadi <jamgbanita@gmail.com>
2022-03-14 15:24:30 -0700

    l

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,7 +17,7 @@ int main(void)
 	printf("Size of a char: %zu byte(s)\n", sizeof(chartype));
 	printf("Size of an int: %zu byte(s)\n", sizeof(inttype));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(lint));
-	printf("Size of a long long int: %llu byte(s)\n", sizeof(llint));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(llint));
 	printf("Size of a float: %zu byte(s)\n", sizeof(floattype));
 	return (0);
 }
