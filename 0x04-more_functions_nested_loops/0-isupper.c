f73251ca6f39ec4d07da37f6975d1ec1bffa6e74
Anita Jamgbadi <jamgbanita@gmail.com>
2022-03-18 15:43:12 +0000

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - contains the isupper() function
+ * to check if a letter islower or upper case
+ * Return: 0 when its true, 1 when its not
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
