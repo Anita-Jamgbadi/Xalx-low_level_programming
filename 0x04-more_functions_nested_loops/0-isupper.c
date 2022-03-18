2329e79a5eeb70d7c847e540420e0da9178b715e
Anita Jamgbadi <jamgbanita@gmail.com>
2022-03-18 15:45:33 +0000

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,6 +3,9 @@
 /**
  * main - contains the isupper() function
  * to check if a letter islower or upper case
+ * _isupper - Actually checkes for the condition 
+ * stated in main
+ * @c: character to be checked
  * Return: 0 when its true, 1 when its not
  */
 
