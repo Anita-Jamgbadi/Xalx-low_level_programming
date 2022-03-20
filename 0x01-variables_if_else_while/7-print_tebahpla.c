6bbf64e60bc097b30803d2b4ec94d06cb415f043
Anita Jamgbadi <jamgbanita@gmail.com>
2022-03-20 16:19:30 +0000

    reverse the lowercase alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet backwards
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	char io;
+
+	for (io = 'z'; io <= 'a'; io--)
+	{
+		putchar(io);
+	}
+	putchar('\n');
+	return (0);
+}
