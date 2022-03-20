ca57574ca0ab47a0dad0109c1121aad0efe272dc
Anita Jamgbadi <jamgbanita@gmail.com>
2022-03-20 15:44:02 +0000

    print lowercase alphabet except e&q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print the lowercase alphabet
+ * except letters q and e
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	char ja;
+
+	for (ja = 'a'; ja <= 'z'; ja++)
+	{
+		if (ja == 'e' || ja == 'q')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(ja);
+		}
+		putchar('\n');
+	}
+	return (0);
+}
