a8d2e530946e817cce5367a0041b727dfb2eadaf
Anita Jamgbadi <jamgbanita@gmail.com>
2022-03-20 16:51:38 +0000

    print 0 to 9 using putchar with spaces and commas inbetween

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print 0 to 9 in ascending order
+ * each digit should be separated with a comma and a space
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
