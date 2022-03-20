536606a33817a8df0deed46f85c893f75c31ff9b
Anita Jamgbadi <jamgbanita@gmail.com>
2022-03-20 18:03:50 +0000

    print lowercase 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10X
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i = 0;
+
+	while (i <= 10)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
