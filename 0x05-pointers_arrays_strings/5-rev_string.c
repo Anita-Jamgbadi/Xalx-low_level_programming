188a475288e9127258df1f80f1feeb63fee4485a
Anita Jamgbadi <jamgbanita@gmail.com>
2022-03-22 23:43:25 +0000

    l

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string -reverses string
+ * @s: holds string to be reversed
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int i, k, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		k = i;
+	}
+
+	while (k > 0)
+	{
+		_putchar(s[k]);
+	}
+}
