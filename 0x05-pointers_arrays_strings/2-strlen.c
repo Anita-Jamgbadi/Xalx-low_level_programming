b6e79f6c8f3acc4bfdac9a3c2efd9bc6c5599f69
Anita Jamgbadi <jamgbanita@gmail.com>
2022-03-22 17:12:29 +0000

    l

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the lenth of a string
+ * @s: string to be scrutinized
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	char ch;
+
+	ch = *s;
+	return (strlen(ch));
+}
