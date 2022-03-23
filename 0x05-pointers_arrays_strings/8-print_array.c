af570c434a165dec3bb7321d4ae975972d811e29
Anita Jamgbadi <jamgbanita@gmail.com>
2022-03-23 00:32:07 +0000

    l

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_array - print n of the length of a
+ * @a: array pointer
+ * @n: number of values to print
+ */
+
+void print_array(int *a, int n)
+{
+	int k, g, b;
+
+	for (k = 0; s[k] != '\0'; k++)
+	{
+		g = k;
+	}
+
+	for (g; g != n; g--)
+	{
+		_putchar("%d, ", a[g]);
+	}
+	_putchar('\n');
+}
