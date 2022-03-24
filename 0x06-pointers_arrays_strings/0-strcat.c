82dd5a34b9ff1cbc342d133f41718961db02adae
Anita Jamgbadi <jamgbanita@gmail.com>
2022-03-24 23:01:07 +0000

    lol

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,19 +12,19 @@ char *_strcat(char *dest, char *src)
 	int len, i, k;
 
 	len = sizeof(dest);
-	
-	for (i = 0; i < sizeof(dest)/ sizeof(char); i++)
+
+	for (i = 0; i <= sizeof(dest) / sizeof(char); i++)
 	{
+		k = 0;
 		if (dest[i] = '\0')
 		{
-			k = 0;
 			*(dest + dest[i] + 1) = src[k];
-			k++;
 		}
 		else
 		{
 		}
+		k++;
 
 	}
-	return (*dest);
+	return (dest);
 }
