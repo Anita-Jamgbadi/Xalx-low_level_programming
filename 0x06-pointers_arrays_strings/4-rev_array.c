8caac35c8321321150a347ef44cb5c37845ddacb
Anita Jamgbadi <jamgbanita@gmail.com>
2022-03-26 01:24:00 +0000

    trying

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,9 +11,10 @@ void reverse_array(int *a, int n)
 {
 	int i, k, start;
 
+	start = 0;
 	k = sizeof(*a) / sizeof(int);
 
-	for (i = k; a[i] != '\0' && i <= 0; i--)
+	for (i = k; a[i] != '\0' && i <= n; i--)
 	{
 		a[start] = a[i];
 	}
