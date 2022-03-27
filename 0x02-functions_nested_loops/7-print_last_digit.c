254e37bfdd371da74d4441664345e2562aad05db
Anita Jamgbadi <jamgbanita@gmail.com>
2022-03-27 23:24:26 +0000

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of input
+ * @a: number to be sructinized
+ * Return: last digit of a
+ */
+
+int print_last_digit(int a)
+{
+	a = a % 10;
+	return (a);
+}
