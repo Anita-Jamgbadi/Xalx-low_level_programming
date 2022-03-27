fc05f098088699a8bf8b89a9fb98a0b2d333d759
Anita Jamgbadi <jamgbanita@gmail.com>
2022-03-27 22:58:38 +0000

    check sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,17 +14,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		i = 1;
-		printf("+");
+		_putchar('+');
 	}
 	else if (n == 0)
 	{
 		i = 0;
-		printf("0");
+		_putchar('0');
 	}
 	else if (n < 0)
 	{
 		i = -1;
-		printf("-");
+		_putchar('-');
 	}
 	return (i);
 }
