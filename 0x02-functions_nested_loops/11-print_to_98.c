1690bae1d8d68c1c0114175c63b1d93da6d28dd4
Anita Jamgbadi <jamgbanita@gmail.com>
2022-03-28 00:34:26 +0000

    print from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints from n to 98;
+ * @n: starting number
+ * Return: void
+ */
+
+void print_to _98(int n)
+{
+	while (n <= 98)
+	{
+		printf("%d, ", n);
+		k++;
+	}
+}
