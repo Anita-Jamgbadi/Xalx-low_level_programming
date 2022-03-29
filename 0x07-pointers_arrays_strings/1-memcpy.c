e6720c06b551e2aaa0c1c4dee6a12f532b0e85e2
Anita Jamgbadi <jamgbanita@gmail.com>
2022-03-29 09:25:57 +0000

    memcopy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies content of src to dest n times
+ * @src: source to copy from
+ * @dest: destination to copy to
+ * @n: number of times to copy
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = *src;
+		i++;
+	}
+	return (dest);
+}
