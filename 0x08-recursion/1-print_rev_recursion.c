7d4a9d5825eee72cd94e382bde3c150100999741
Anita Jamgbadi <jamgbanita@gmail.com>
2022-03-30 15:41:09 +0000

    rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,22 +1,41 @@
 #include "main.h"
 
 /**
- * print_rev_recursion - print a string in reverse
+ * _print_rev_recursion - print a string in reverse
+ * getEnd - gets to the end of a string
  * @s: points to the string to be printed
  * Return: void
  */
 
-void _print_rev_recursion(char *s)
+
+char getEnd(char *s)
 {
-	if (*s == '\0')
-	{
-		s--;
-		_putchar(*s);
-		_print_rev_recursion(s);
-	}
 	if (*s != '\0')
 	{
 		s++;
-		_print_rev_recursion(s);
 	}
+	return (*s);
+}
+
+void _print_rev_recursion(char *s)
+{
+	char *res;
+	char **tmp;
+
+	tmp = &s;
+
+	getEnd(s);
+
+	if (**tmp == *s)
+	{
+		_putchar(*s);
+	}
+
+	if (*s == '\0')
+	{
+		res = s--;
+		_putchar(*s);
+		_print_rev_recursion(res);
+	}
+
 }
